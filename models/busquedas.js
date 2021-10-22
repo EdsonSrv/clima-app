1c894f1ecb66d24de2531b1e76284440bc74adb6
Edson Srvn <edson@ciencias.unam.mx>
2021-10-22 17:18:56 -0500

    Adding MAPBOX end point

--- models/busquedas.js
+++ models/busquedas.js
@@ -10,11 +10,15 @@ class Busquedas {
   }
 
   async ciudad( lugar = '' ){
-    //Petición http con AXIOS
-    const resp = await axios.get('https://reqres.in/api/users?page=2');
-    console.log(resp.data);
-
-    return []; //regresar los lugares
+    try{
+      //Petición http con AXIOS
+      const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/CDMX%2C%20Ciudad%20de%20M%C3%A9xico%2C%2004120%2C%20M%C3%A9xico.json?language=es&access_token=pk.eyJ1IjoiZWRzb25zcnZuIiwiYSI6ImNrdjJ3eW5heTBreHcyb25ubTRwODM0d2gifQ.5lYwNENvHmWqGQ7BM0dd4Q');
+      console.log(resp.data);
+  
+      return []; //regresar los lugares
+    }catch (error) {
+      return [];
+    }
   }
 }
 
