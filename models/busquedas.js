37e396073d73539b597d4993289a369193e14623
Edson Srvn <edson@ciencias.unam.mx>
2021-10-23 17:08:27 -0500

    Add place data mapping

--- models/busquedas.js
+++ models/busquedas.js
@@ -26,9 +26,13 @@ class Busquedas {
       });
       // const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/CDMX%2C%20Ciudad%20de%20M%C3%A9xico%2C%2004120%2C%20M%C3%A9xico.json?language=es&access_token=pk.eyJ1IjoiZWRzb25zcnZuIiwiYSI6ImNrdjJ3eW5heTBreHcyb25ubTRwODM0d2gifQ.5lYwNENvHmWqGQ7BM0dd4Q');
       const resp = await instance.get();
-      console.log(resp.data);
   
-      return []; //regresar los lugares
+      return resp.data.features.map( lugar => ({
+        id: lugar.id,
+        nombre: lugar.place_name,
+        lng: lugar.center[0],
+        lat: lugar.center[1]
+      }));
     }catch (error) {
       return [];
     }
