f54d3582a5cd612311b349c6426143455a9626a6
Edson Srvn <edson@ciencias.unam.mx>
2021-10-25 14:55:13 -0500

    Completing weather data

--- index.js
+++ index.js
@@ -28,15 +28,18 @@ const main = async() => {
         // console.log({lugarSel});
 
         //Clima
+        const clima = await busquedas.climaLugar( lugarSel.lat, lugarSel.lng );
 
         //Mostrar resultados
+        console.clear();
         console.log('\nInformación de la ciudad\n'.green);
         console.log('Ciudad:', lugarSel.nombre);
         console.log('Lat:', lugarSel.lat);
         console.log('Lng:', lugarSel.lng);
-        console.log('Temperatura:',);
-        console.log('Mínima:',);
-        console.log('Máxima:',);
+        console.log('Temperatura:', clima.temp);
+        console.log('Mínima:', clima.min);
+        console.log('Máxima:', clima.max);
+        console.log('Como está el clima:', `${clima.desc}`.green);
 
         break;
     
